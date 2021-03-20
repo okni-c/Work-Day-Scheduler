3eb73ded0dc6b01ccfb1d30b2d5e7f62545e5441
okni-c <oknistay@gmail.com>
2021-03-20 17:41:48 -0500

    v2.0 - array saving technique finished

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,48 @@
 let today = moment();
 $("#currentDay").text(today.format('dddd, MMMM Do'));
 
-var boxData = $("#event-box").val();
+var elementArr = [document.getElementById("e0"), document.getElementById("e1"), document.getElementById("e2"), document.getElementById("e3"), document.getElementById("e4"), document.getElementById("e5"), document.getElementById("e6"), document.getElementById("e7"), document.getElementById("e8")];
 
-var boxDataEl = document.getElementById("event-box")
-
-function renderLastRegistered() {
+function renderLastRegistered(arr) {
     // Retrieve the last event-box from localStorage using `getItem()`
-    var boxDataSaved = localStorage.getItem('boxData');
-
-    // If it is null, return early from this function
-    if (boxDataSaved === null) {
-        return;
+    var i = 0;
+    while (i <= 8) {
+        //get from local storage
+        var elementString = localStorage.getItem('boxData' + i);
+        // console.log(arr[i]);
+        elementArr[i].textContent = elementString;
+        i++;
     }
 
-    // Set the text of the 'boxDataEl' to the corresponding values from localStorage
-    boxDataEl.textContent = boxDataSaved;
+    // If it is null, return early from this function
+    if (elementString === null) {
+        return;
+    }
 }
 
 renderLastRegistered();
 
-//create event listener for save button for this function
-//function to save contents of textarea to localStorage
-$("#save-button").click(function () {
+//when any save button is pressed
+$("#save-button1, #save-button2, #save-button3, #save-button4, #save-button5, #save-button6, #save-button7, #save-button8, #save-button9").click(function () {
 
-    var newboxData = $("#event-box").val();
+    var arr = [];
 
-    // Save event-box to localStorage using `setItem()`
-    localStorage.setItem('boxData', newboxData);
+    arr.splice(0, arr.length)
 
-    renderLastRegistered();
+    var i = 0;
+    while (i <= 8) {
+        //grab data from each event-box
+        var elementString = $("#e" + i).val();
+        //push to array
+        arr.push(elementString);
+        console.log(arr[i]);
+        //save to local storage
+        localStorage.setItem('boxData' + i, elementString);
+        i++;
+    }
+
+    renderLastRegistered(arr);
 
 });
 
-
-
-
-
-
 //create function to change timeblock class depending on time using moment.js
-
